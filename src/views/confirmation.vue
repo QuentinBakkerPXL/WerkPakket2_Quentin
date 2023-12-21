98436e93d82ec8ac691846260cc266e6e9585566
QuentinBakkerPXL <114091462+QuentinBakkerPXL@users.noreply.github.com>
2023-12-21 22:40:47 +0100

    Done?

--- src/views/confirmation.vue
+++ src/views/confirmation.vue
@@ -19,8 +19,6 @@ export default {
     const address = ref('');
 
     const confirmOrder = () => {
-      // Here you can handle the order confirmation logic
-      // For example, saving the order details to a server
       alert("Order confirmed with address: " + address.value);
       router.push('/thankyou');
     };
@@ -52,8 +50,11 @@ export default {
       </div>
 
       <div class="address-form">
-        <h3>Enter Shipping Address:</h3>
-        <input type="text" v-model="address" placeholder="Shipping Address">
+        <div class="txt_field">
+          <input type="text" class="adresss" v-model="address" placeholder="Shipping Address">
+          <span></span>
+          <label>Enter shipping adress</label>
+        </div>
         <button @click="confirmOrder">Confirm Order</button>
       </div>
     </div>
@@ -69,10 +70,16 @@ export default {
   margin: auto;
   padding: 20px;
 }
-
+h2 {
+  margin: 20px 0 20px 0;
+}
+h3 {
+  margin: 10px 0 10px 0;
+}
 .order-details, .address-form {
   margin-bottom: 20px;
 }
+
 
 ul {
   list-style-type: none;
@@ -81,5 +88,69 @@ ul {
 
 li {
   margin: 5px 0;
+}
+
+.address-form .txt_field {
+  position: relative;
+  border-bottom: 2px solid #adadad;
+  margin: 30px 0;
+}
+
+.address-form .txt_field input {
+  width: 100%;
+  padding: 0 5px;
+  height: 40px;
+  font-size: 16px;
+  border: none;
+  background: none;
+  outline: none;
+}
+
+.address-form .txt_field label {
+  position: absolute;
+  top: 50%;
+  left: 5px;
+  color: #adadad;
+  transform: translateY(-50%);
+  font-size: 16px;
+  pointer-events: none;
+  transition: 0.5s;
+}
+
+.address-form .txt_field span::before {
+  content: '';
+  position: absolute;
+  top: 40px;
+  left: 0;
+  width: 0;
+  height: 2px;
+  background: #5b95d4;
+  transition: 0.5s;
+}
+
+.address-form .txt_field input:focus ~ label,
+.address-form .txt_field input:valid ~ label {
+  top: -5px;
+  color: #5b95d4;
+}
+
+.address-form .txt_field input:focus ~ span::before,
+.address-form .txt_field input:valid ~ span::before {
+  width: 100%;
+}
+
+button {
+  width: 150px;
+  border-radius: 25px;
+  border: none;
+  padding: 10px;
+  font-size: 0.9rem;
+  background-color: #adadad;
+  color: white;
+  margin: 30px 0 30px 0;
+}
+button:hover {
+  background-color: #c76744;
+  color: white;
 }
 </style>
